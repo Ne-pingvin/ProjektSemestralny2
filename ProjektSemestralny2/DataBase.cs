4e06abe0dff4d02f8fad4488d349795d934d463f
Ne-pingvin <anathema.an@gmail.com>
2024-06-12 17:36:45 +0200

    Add project files.

---
+++ ProjektSemestralny2/DataBase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ProjektSemestralny2
+{
+    internal class DataBase
+    {
+        static string connectionString = @"Data Source=LAPTOP-472C2EDF;Initial Catalog=VotesOnline;Integrated Security=True";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        public void OpenConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+                sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+                sqlConnection.Close();
+        }
+        public SqlConnection GetConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
